52bb225daa7b0107b5b96ac4f353112d97b30e0d
STBenji <michaelgsg16@gmail.com>
2023-03-27 22:53:16 -0500

    test

--- src/routes/historial.routes.js
+++ src/routes/historial.routes.js
@@ -7,6 +7,6 @@ const router = new Router();
 // Add routes
 
 router.get('/user/historial', getHistorial);
-router.get('/user/historial', getHistorialPorFechas );
+router.get('/user/historial-fecha', getHistorialPorFechas );
 
 export default router
